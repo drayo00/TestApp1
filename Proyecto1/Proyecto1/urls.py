4461f90bdb446b713adf4fe92b148cb74e8886f3
drayo00 <diego.reyes.a@ug.uchile.cl>
2024-08-28 01:28:44 -0400

    feat: Implement some urls and views

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name="Home"),
+    path('servicios',views.servicios, name="Servicios"),
+    path('tienda',views.tienda, name="Tienda"),
+    path('contacto',views.contacto, name="Contacto"),
 ]
